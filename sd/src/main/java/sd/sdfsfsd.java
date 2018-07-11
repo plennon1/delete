ce798f7765fca7c3e04b3a2ee6389f82c6ab5bd5
Paul L <plennon@ait.ie>
2018-07-11 11:50:22 +0100

    Core files

---
+++ sd/src/main/java/sd/sdfsfsd.java
@@ -0,0 +1,15 @@
+package sd;
+
+public class sdfsfsd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void add(int a, int b)
+	{
+		int total = a + b;
+	}
+
+}
---
+++ sd/src/test/java/sd/TestSd.java
@@ -0,0 +1,24 @@
+package sd;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSd {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
